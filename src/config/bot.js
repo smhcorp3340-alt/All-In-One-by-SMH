cedccdab55c282c2643482522df6fb74e1ad87c7
smhcorp3340-alt <smhcorp3340@gmail.com>
2026-06-27 17:50:26 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "The bot All-In-One by SMH !",
+        name: "maman lpb et maude lpb aussi",
         // Activity type number (0 = Playing).
         type: 0,
       },
